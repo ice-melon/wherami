ca85c7c06791fe6e8ea1bd41a5fd05c4c5e62880
ice-melon <chunjiang2010@gmail.com>
2014-08-13 11:57:23 +0800

    change email setting

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,13 @@ require File.expand_path('../application', __FILE__)
 SampleApp::Application.initialize!
 
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :domain         => 'mail.google.com',
-    :port           => 587,
-    :user_name      => 'wherami.mtrec@gmail.com',
-    :password       => 'mtrec2014',
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#     :address        => 'smtp.gmail.com',
+#     :domain         => 'mail.google.com',
+#     :port           => 587,
+#     :user_name      => 'wherami.mtrec@gmail.com',
+#     :password       => 'mtrec2014',
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+# }
